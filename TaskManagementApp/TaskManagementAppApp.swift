e564a34d22dec83d436357782a67ef20dfa973eb
Om Gandhi <omgandhi255@gmail.com>
2025-06-25 13:01:17 +0530

    Initial Commit

---
+++ TaskManagementApp/TaskManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementAppApp.swift
+//  TaskManagementApp
+//
+//  Created by Om Gandhi on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskManagementAppTests/TaskManagementAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TaskManagementAppTests.swift
+//  TaskManagementAppTests
+//
+//  Created by Om Gandhi on 25/06/25.
+//
+
+import Testing
+
+struct TaskManagementAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
